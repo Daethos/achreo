137fc5143ec89e2e33836673ae2ca3ddda69f6bc
elijiah silvereagle <elijiahsilvereagle@gmail.com>
2025-04-11 10:34:41 -0700

    Boost Screen Shake

--- src/game/phaser/ScreenShake.ts
+++ src/game/phaser/ScreenShake.ts
@@ -11,21 +11,20 @@ export function hitStop(scene: Phaser.Scene, duration = 32) {
     }, duration);
 };
 
-export function screenShake(scene: Phaser.Scene, duration = 32, intensity = 0.002) {
-    totalTrauma += 1.02;
+export function screenShake(scene: Phaser.Scene, duration = 32, intensity = 0.003) {
+    totalTrauma += 1.03;
     intensity *= Math.pow(totalTrauma, 2);
-
     if ("vibrate" in navigator && navigator?.vibrate !== undefined) navigator.vibrate(duration);
     const decayInterval = setInterval(() => {
         scene.cameras.main.shake(duration, intensity);
-        totalTrauma -= 1.02 / duration;
+        totalTrauma -= 1.03 / duration;
         if (totalTrauma <= 0) {
             totalTrauma = 0;
             clearInterval(decayInterval);
         };
     }, 16);
 };
-export function sprint(scene: Phaser.Scene, duration = 32, intensity = 0.00125) { // 48 || 0.0004
+export function sprint(scene: Phaser.Scene, duration = 32, intensity = 0.002) { // 48 || 0.0004
     scene.cameras.main.shake(duration, intensity);
 };
 export function walk(scene: any, duration = 32, intensity = 0.000675) { // 32 || 0.0003
